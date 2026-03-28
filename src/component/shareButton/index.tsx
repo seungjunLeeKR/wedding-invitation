6135aa8be4e82b36dd7c336c4c4f925cea2d91da
sjlee <byjun3992@naver.com>
2026-03-28 20:47:45 +0900

    변경 사항:
    feed 타입 → list 타입으로 변경
    buttons 배열에 2개 버튼 추가: "청첩장 보기" + "공유하기"

--- src/component/shareButton/index.tsx
+++ src/component/shareButton/index.tsx
@@ -26,22 +26,33 @@ export const ShareButton = () => {
           const shareUrl = window.location.protocol + "//" + window.location.host + baseUrl
           
           kakao.Share.sendDefault({
-            objectType: "commerce",
-            content: {
-              title: `${GROOM_FULLNAME} ❤️ ${BRIDE_FULLNAME}의 결혼식에 초대합니다.`,
-              description: WEDDING_DATE.format(WEDDING_DATE_FORMAT) + "\n" + LOCATION,
-              imageUrl: shareUrl + "/preview_image.png",
-              link: {
-                mobileWebUrl: shareUrl,
-                webUrl: shareUrl,
+            objectType: "list",
+            headerTitle: `${GROOM_FULLNAME} ❤️ ${BRIDE_FULLNAME}의 결혼식`,
+            headerLink: {
+              mobileWebUrl: shareUrl,
+              webUrl: shareUrl,
+            },
+            contents: [
+              {
+                title: "청첩장",
+                description: WEDDING_DATE.format(WEDDING_DATE_FORMAT) + "\n" + LOCATION,
+                imageUrl: shareUrl + "/preview_image.png",
+                link: {
+                  mobileWebUrl: shareUrl,
+                  webUrl: shareUrl,
+                },
               },
-            },
-            commerce: {
-              regularPrice: 0,
-            },
+            ],
             buttons: [
               {
                 title: "청첩장 보기",
+                link: {
+                  mobileWebUrl: shareUrl,
+                  webUrl: shareUrl,
+                },
+              },
+              {
+                title: "공유하기",
                 link: {
                   mobileWebUrl: shareUrl,
                   webUrl: shareUrl,
