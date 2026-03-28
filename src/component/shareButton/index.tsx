d9c808c5f6f5c588d5a717130d5a2df1230258e9
sjlee <byjun3992@naver.com>
2026-03-28 21:32:33 +0900

    Refactor ShareButton to enhance sharing functionality with template arguments for personalized messages

--- src/component/shareButton/index.tsx
+++ src/component/shareButton/index.tsx
@@ -1,10 +1,3 @@
-import {
-  BRIDE_FULLNAME,
-  GROOM_FULLNAME,
-  LOCATION,
-  WEDDING_DATE,
-  WEDDING_DATE_FORMAT,
-} from "../../const"
 import ktalkIcon from "../../icons/ktalk-icon.png"
 import { LazyDiv } from "../lazyDiv"
 import { useKakao } from "../store"
@@ -13,6 +6,7 @@ const baseUrl = import.meta.env.BASE_URL
 
 export const ShareButton = () => {
   const kakao = useKakao()
+
   return (
     <LazyDiv className="footer share-button">
       <button
@@ -24,15 +18,17 @@ export const ShareButton = () => {
             return
           }
 
+          // 현재 페이지 주소 (공유할 URL)
           const shareUrl = window.location.protocol + "//" + window.location.host + baseUrl
           
           try {
+            // 템플릿 ID만 지정해서 호출 (내용은 템플릿 설정이나 페이지의 OG 태그를 따름)
             if (kakao.Share && kakao.Share.sendScrap) {
               kakao.Share.sendScrap({
                 requestUrl: shareUrl,
+                templateId: 131174, // 생성하신 ID
               })
             } else {
-              // sendScrap 미지원 시 링크 복사
               navigator.clipboard.writeText(shareUrl)
               alert("공유 링크가 복사되었습니다.\n" + shareUrl)
             }
